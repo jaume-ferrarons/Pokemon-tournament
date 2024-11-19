de272d7411b3d5d293e925f57110cf19635cfe87
hectormirete <hectormirete@gmail.com>
2024-11-19 21:18:45 +0100

    rename team

--- teams/hector/select_move.py
+++
@@ -1,9 +0,0 @@
-from models.pokemon import Pokemon
-from models.movement import Movement
-from typing import List
-
-
-def select_move(attacker_moves: List[Movement], attacker:Pokemon, defender: Pokemon):
-    if defender.attack > defender.sp_attack:
-        buff
-    return attacker_moves[0]
